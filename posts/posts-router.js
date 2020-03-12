9fdbd9549d3a6e3ea912222688bb79737582ffa3
Amber Mohler <ambermohler@iMac.local>
2020-03-12 10:26:12 -0400

    completed mvp

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -79,16 +79,42 @@ router.get('/:id/comments', (req, res) => {
         })
         .catch(error => {
             console.log(error);
-            res.status(500).json({ error: "The comments information could not be retrieved" })
+            res.status(500).json({ error: "The comments information could not be retrieved" });
         })
 })
 
 router.delete('/:id', (req, res) => {
-
+    const id = req.params.id;
+    Posts.remove(id)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+                res.status(200).json({ message: "Post has been successfully deleted" });
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ message: "The post could not be removed" });
+        })
 })
 
 router.put('/:id', (req, res) => {
-
+    const id = req.params.id;
+    Posts.update(id, req.body)
+        .then(post => {
+            if (!id) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else if (!req.body.title || !req.body.contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post" });
+            } else if (post > 0) {
+                res.status(200).json(req.body);
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: "The post information could not be modified"})
+        })
 })
 
 
